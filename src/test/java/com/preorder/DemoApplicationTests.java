227138bd40a4d6ec5aca6d16751328968d1c034d
hyunjeong <hyunjeong9592@gmail.com>
2024-04-21 15:10:00 +0900

    Initial commit

---
+++ src/test/java/com/preorder/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
