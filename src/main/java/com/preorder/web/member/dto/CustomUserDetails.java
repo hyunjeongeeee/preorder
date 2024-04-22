0b5fdeb83506f230d0267be89536d9775f8f7ab2
hyunjeong <hyunjeong9592@gmail.com>
2024-04-22 23:33:40 +0900

    feat: implementation of login function

---
+++ src/main/java/com/preorder/web/member/dto/CustomUserDetails.java
@@ -0,0 +1,63 @@
+package com.preorder.web.member.dto;
+
+import com.preorder.domain.member.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return String.valueOf(member.getRole());
+            }
+        });
+
+        return collection;
+    }
+
+
+
+    @Override
+    public String getPassword() {
+        return member.getMemberPw();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getMemberId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/preorder/web/member/jwt/LoginFilter.java
@@ -0,0 +1,53 @@
+package com.preorder.web.member.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        // 클라이언트 요청에서 id, pw  추출
+        String id = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        System.out.println("아이디 : " + id);
+        // 스프링 시큐리티에서는 id, pw 검증을 위해 token에 담아야함
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(id, password, null);
+
+        // token에 담은 검증을 위한 AuthenticationManager로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    // 로그인 성공시 실행 메소드 (JWT 토큰 발급)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        System.out.println("success");
+    }
+
+    // 로그인 실패 시행 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        System.out.println("fail");
+    }
+
+
+
+}
