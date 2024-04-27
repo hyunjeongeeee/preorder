8c4e8f8747fad21dc66b2f1ed4bc7bd20385f7ca
hyunjeong <hyunjeong9592@gmail.com>
2024-04-27 15:03:47 +0900

    remove: Delete unnecessary files
    중복파일, 불필요한 파일 정리

--- src/main/java/com/preorder/web/cart/repository/WishListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.preorder.web.cart.repository;
-
-import com.preorder.domain.cart.WishList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WishListRepository extends JpaRepository<WishList, Integer> {
-}
