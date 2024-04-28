70b9bf032362a8425e2a677c7da1f6896a968143
hyunjeong <hyunjeong9592@gmail.com>
2024-04-28 20:30:02 +0900

    Refactor: Edit email settings file
    이메일 설정파일 정보 수정

--- src/main/java/com/preorder/web/email/config/EmailConfig.java
+++ src/main/java/com/preorder/web/email/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.preorder.web.email.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,19 +12,21 @@ import java.util.Properties;
 public class EmailConfig {
 
     @Bean
-    public JavaMailSender NaverMailService(){
+    public JavaMailSender GmailMailService(@Value("${spring.mail.host}") String host,
+                                           @Value("${spring.mail.username}") String username,
+                                           @Value("${spring.mail.password}") String password,
+                                           @Value("${spring.mail.port}") int port) {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-
-
-        javaMailSender.setPort(465); // SMTP 포트
-
-        javaMailSender.setJavaMailProperties(getMailProperties()); // 메일 인증서버 가져오기
-
+        javaMailSender.setHost(host);   // SMTP 서버명
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        javaMailSender.setPort(port);
+        javaMailSender.setJavaMailProperties(getMailProperties());  // 메일 인증서버 가져오기
         return javaMailSender;
     }
-
+    @Bean
     // 메일 인증서버 정보 가져오기
-    private Properties getMailProperties(){
+    public Properties getMailProperties(){
         Properties properties = new Properties();
         properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
         properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
@@ -31,7 +34,6 @@ public class EmailConfig {
         properties.setProperty("mail.debug", "true"); // 디버그 사용
         properties.setProperty("mail.smtp.ssl.trust", "smtp.gmail.com"); // ssl 인증 서버 (smtp 서버명)
         properties.setProperty("mail.smtp.ssl.enable", "true"); // ssl 사용
-
         return properties;
     }
 }
--- src/main/java/com/preorder/web/email/service/RegisterMail.java
+++ src/main/java/com/preorder/web/email/service/RegisterMail.java
@@ -1,4 +1,4 @@
-package com.preorder.web.email.sercive;
+package com.preorder.web.email.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
